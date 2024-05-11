b62ede97c961233ac04f5eb7b58f2769b315767c
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-05-10 21:55:37 -0400

    Basic layout down for barebonse game

---
+++ script.js
@@ -0,0 +1,39 @@
+// const countDiv = document.querySelector(".incrementCount")
+// const increaseCountBtn = document.querySelector(".increaseCount")
+// const increaseCountMultiplierBtn = document.querySelector(".increaseCountMultiplier")
+// const increaseNumberDisplay = document.querySelector("#countDisplay")
+// const increaseCostUpgradeDisplay = document.querySelector("#costDisplay")
+
+// let count = 0;
+// let clickMultiplier = 1;
+// let costUpgrade = 20;
+// increaseNumberDisplay.innerHTML = clickMultiplier
+// increaseCostUpgradeDisplay.innerHTML = costUpgrade
+// function updateCount(){
+//     count ++;
+//     countDiv.innerHTML = count
+// }
+// function checkCount(){
+//     localStorage.setItem("countNumber", count)
+// }
+
+// increaseCountBtn.addEventListener("click", function (){
+//     count = count + (1*clickMultiplier);
+//     countDiv.innerHTML = count
+    
+// } )
+
+// increaseCountMultiplierBtn.addEventListener("click", function (){
+//     if(count >= costUpgrade){
+//         clickMultiplier ++;
+//         increaseNumberDisplay.innerHTML = clickMultiplier
+//         count = count - costUpgrade
+//         countDiv.innerHTML = count
+//         increaseCostUpgrade()
+//         increaseCostUpgradeDisplay.innerHTML = costUpgrade
+//     }
+// } )
+
+// function increaseCostUpgrade(){
+//     costUpgrade = Math.ceil(costUpgrade * 1.5);
+// }
